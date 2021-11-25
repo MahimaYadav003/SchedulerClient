0e9c100868c7c8faf1bf6b94ab399ea0a3a55cb8
MahimaYadav003 <mahimayadav_18041@aitpune.edu.in>
2021-11-25 21:57:58 +0530

    added deploy springboot application url

--- src/api/baseUrls.js
+++ src/api/baseUrls.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 export const SCHEDULER_HOST="https://schedulerspringboot.herokuapp.com/scheduler";
-=======
-export const SCHEDULER_HOST="https://schedulerspringboot.herokuapp.com";
->>>>>>> 775854e253dbe27b0dd86ae2614fec75f59f8d56
